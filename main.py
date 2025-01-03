40e676c42b08686dad4feb9a29a8084b649d77ed
Tasso-7 <giova.tasselli@gmail.com>
2025-01-03 10:53:41 +0000

    Implementation of stateGenerator function that generate the set of all states. It also generates lost states. Return empty best move and evaluation set. empty expect for lost states.

--- main.py
+++ main.py
@@ -8,14 +8,11 @@ class state:
     a1, a2, b1, b2 = 1, 1, 1, 1 # hands of both players in decreasing order
     def __init__(self, a1, a2, b1, b2):
         a = [a1,a2]
-        a.sort(reverse=True)
         b = [b1,b2]
+        a.sort(reverse=True)
         b.sort(reverse=True)
-        self.a1 = a[0]
-        self.a2 = a[1]
-
-        self.b1 = b[0]
-        self.b2 = b[1]
+        self.a1, self.a2 = a[0], a[1]
+        self.b1, self.b2 = b[0], b[1]
 
     def __eq__(self, other):
         if not isinstance(other, state):
@@ -46,3 +43,30 @@ class state:
                     [self.b2,self.b1][j],self.a1,self.a2)) # values that remain unchanged
         
         return remove_repetitions(ADJs)
+    
+
+def stateGenerator():
+    nOfConfings = (N_OF_FINGERS*(N_OF_FINGERS+1)/2)**2 # N of states the game can be in 
+    nOfConfings = int(nOfConfings)
+
+    States = [] # set of all states
+    bestMoves = [" " for i in range(0, nOfConfings)] # best move for every state
+    Values = [" " for i in range(0, nOfConfings)] # value of every state [-1;1]
+
+    j = 0
+    for a in range(0, N_OF_FINGERS, 1):
+        for b in range(a + 1):
+            for c in range(0, N_OF_FINGERS, 1):
+                for d in range(c + 1):
+                    States.append(state(a,b,c,d)) # Generating states
+                    if a == b and a == 0: # it's a lost game
+                        Values[j] = -1
+                        bestMoves[j] = []
+                    j+=1
+    return (States, Values, bestMoves) # a " " means unkown
+
+if __name__ == "__main__":
+    print("WELCOME TO FINGER'S GAME ANALYSIS")
+    for i in stateGenerator():
+        print(i)
+    
